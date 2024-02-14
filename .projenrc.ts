379f3059cdd94b9d705cb308587e7b2ff4470e51
Paulo Pereira <paulospj@amazon.com>
2024-02-14 17:23:48 +0000

    chore: changed project properties

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,28 @@
 import { awscdk } from 'projen';
+import { DependabotScheduleInterval } from 'projen/lib/github/dependabot';
+import { ProjectType } from 'projen/lib/project';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Paulo Pereira',
   authorAddress: 'paulospj@amazon.com',
   cdkVersion: '2.1.0',
+  projectType: ProjectType.LIB,
+  deps: ['@aws-cdk/core'],
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
-  name: 'construct-s3-athena-glue',
+  name: 'cdk-s3-athena-glue',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/paulospj/construct-s3-athena-glue.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: 'https://github.com/paulosergio-jnr/cdk-s3-athena-glue.git',
+  gitignore: [
+    '.idea',
+  ],
+  docgen: true,
+  eslint: true,
+  dependabot: true,
+  dependabotOptions: {
+    ignoreProjen: true,
+    scheduleInterval: DependabotScheduleInterval.WEEKLY,
+  },
 });
+
 project.synth();
